df79993c8711b7d03a669a3c5c8b6311fc1af3a9
Jason Dijols <jasondijols@gmail.com>
2026-04-07 11:26:55 -0500

    Fix port number

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
